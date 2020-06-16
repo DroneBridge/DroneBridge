d4b7c58abd70df08bbb242862fc4419dfcfd2bf7
seeul8er <24637325+seeul8er@users.noreply.github.com>
2020-06-16 21:08:49 +0200

    Added write buffer for recorder

--- recorder/recorder.c
+++ recorder/recorder.c
@@ -1,7 +1,7 @@
 /*
  *   This file is part of DroneBridge: https://github.com/seeul8er/DroneBridge
  *
- *   Copyright 2019 Wolfgang Christl
+ *   Copyright 2020 Wolfgang Christl
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -34,8 +34,9 @@
 
 #define MAX_LEN_FILENAME    64
 #define MAX_LEN_FILEPATH    512
-#define REC_BUFF_SIZE       819200    // ~6Mbit
+#define REC_BUFF_SIZE       819200  // ~6Mbit
 #define MAX_RETRY_UNIX      100
+#define MIN_WRITE_BUFF_LEN  10240   // minimum size of buffered data before fwrite
 
 typedef struct {
     FILE *file_pnt;
@@ -91,12 +92,15 @@ void delete_file(video_file_t *video_file) {
         LOG_SYS_STD(LOG_INFO, "DB_RECORDER: Unable to delete empty file %s > %s!\n", video_file->filepath, strerror(errno));
 }
 
-void write_to_file(video_file_t *video_file, uint8_t *rec_buff, size_t num_new_bytes) {
+bool write_to_file(video_file_t *video_file, uint8_t *rec_buff, size_t num_new_bytes) {
     if ((*video_file).file_pnt != NULL) {
         ssize_t written = fwrite(rec_buff, (int) num_new_bytes, 1, (*video_file).file_pnt);
-        if (written != 1)
+        if (written != 1) {
             LOG_SYS_STD(LOG_WARNING, "DB_RECORDER: Not all data written to file (%ld/%ld)\n", written, num_new_bytes);
+        } else
+            return true;
     }
+    return false;
 }
 
 /**
@@ -109,6 +113,7 @@ void write_to_file(video_file_t *video_file, uint8_t *rec_buff, size_t num_new_b
 int main(int argc, char *argv[]) {
     signal(SIGINT, int_handler);
     signal(SIGTERM, int_handler);
+    signal(SIGKILL, int_handler);
 
     char record_dir[MAX_LEN_FILEPATH-MAX_LEN_FILENAME];
     if (strlen(argv[1]) == 0)
@@ -147,14 +152,32 @@ int main(int argc, char *argv[]) {
         close(domain_sock);
         return -1;
     }
-    
+
+    fd_set fd_socket_set;
+    struct timeval select_timeout;
     uint8_t rec_buff[REC_BUFF_SIZE];
+    ssize_t len_buffered = 0;
     while (recorder_running) {
-        size_t num_new_bytes = recv(domain_sock, rec_buff, REC_BUFF_SIZE-1, 0);
-        if (num_new_bytes > 0) {
-            write_to_file(&video_file, rec_buff, num_new_bytes);
+        // select with timeout to be able to safely kill the recorder writing all remaining data
+        select_timeout.tv_sec = 1;
+        select_timeout.tv_usec = 0;
+        FD_ZERO(&fd_socket_set);
+        FD_SET(domain_sock, &fd_socket_set);
+        int select_return = select(domain_sock + 1, &fd_socket_set, NULL, NULL, &select_timeout);
+        if (select_return > 0) {
+            if (FD_ISSET(domain_sock, &fd_socket_set)) {
+                len_buffered += recv(domain_sock, &rec_buff[len_buffered], REC_BUFF_SIZE - len_buffered, 0);
+                if (len_buffered >= MIN_WRITE_BUFF_LEN) {
+                    if (write_to_file(&video_file, rec_buff, len_buffered))
+                        len_buffered = 0;
+                }
+            }
+        } else if(select_return == -1) {
+            if (errno != EINTR) // ignore kill event
+                LOG_SYS_STD(LOG_WARNING, "DB_RECORDER: Select returned an error > %s\n", strerror(errno));
         }
     }
+    write_to_file(&video_file, rec_buff, len_buffered);
     close(domain_sock);
     fclose(video_file.file_pnt);
     LOG_SYS_STD(LOG_INFO, "DB_RECORDER: Terminated!\n");
